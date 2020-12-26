7c8d3550680f7601e15f98763ed491b2b2fd7ca8
Zhihao Lin <zhihaolin1005@gmail.com>
2020-12-26 20:28:57 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,21 +1,7 @@
 require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
-  end
-  if int % 5 == 0
-    "Buzz"
-  end
-  if int % 15 == 0
-    "FizzBuzz"
-  end
-  if int % 3 != 0
-    "nil"
-  end
-  if int % 5 != 0
-    "nil"
-  end
+
 end
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
