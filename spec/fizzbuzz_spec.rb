b7f22e880fc64622509ca06a16f00619c0db1993
Zhihao Lin <zhihaolin1005@gmail.com>
2020-12-26 20:26:29 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,19 @@
 require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
 def fizzbuzz(int)
-  if int % 3 == 1
+  if int % 3 == 0
     "Fizz"
   end
-  if int % 5 == 1
+  if int % 5 == 0
     "Buzz"
   end
-  if int % 15 == 1
+  if int % 15 == 0
     "FizzBuzz"
   end
-  if int % 3 != 1
+  if int % 3 != 0
     "nil"
   end
-  if int % 5 != 1
+  if int % 5 != 0
     "nil"
   end
 end
